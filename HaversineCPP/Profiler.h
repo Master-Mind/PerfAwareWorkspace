7922d22cf47d1d860de41de93568bf2669022e85
Master-Mind <philip.a.hollingsworth@gmail.com>
2026-01-01 14:43:51 -0500

    Made c++ implementation
    Worked up to the page fault section of the course

---
+++ HaversineCPP/Profiler.h
@@ -0,0 +1,203 @@
+#pragma once
+#ifdef _WIN32
+#include <intrin.h>
+#include <windows.h>
+#include <Psapi.h>
+#else
+#include <x86intrin.h>
+#endif
+
+#include <mutex>
+
+typedef unsigned long long u64;
+
+inline u64 GuesstimateCPUFrequency()
+{
+	auto dur = std::chrono::duration(std::chrono::milliseconds(100));
+	auto start = std::chrono::high_resolution_clock::now();
+	u64 nowCycles = __rdtsc();
+
+	while (std::chrono::high_resolution_clock::now() - start < dur);
+
+	u64 afterCycles = __rdtsc();
+
+	return (afterCycles - nowCycles) * 10;
+}
+
+struct OSHandler
+{
+	OSHandler()
+	{
+		procHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, GetCurrentProcessId());
+	}
+
+	u64 GetPageFaults()
+	{
+		PROCESS_MEMORY_COUNTERS_EX mem;
+		mem.cb = sizeof(PROCESS_MEMORY_COUNTERS_EX);
+
+		GetProcessMemoryInfo(procHandle, reinterpret_cast<PPROCESS_MEMORY_COUNTERS>(&mem), sizeof(mem));
+
+		return mem.PageFaultCount;
+	}
+	HANDLE procHandle;
+};
+
+inline std::string humanSize(u64 bytes)
+{
+	//adapted from https://gist.github.com/dgoguerra/7194777
+	const char* suffix[] = { "B", "KB", "MB", "GB", "TB" };
+	const char length = sizeof(suffix) / sizeof(suffix[0]);
+
+	int i = 0;
+	double dblBytes = static_cast<double>(bytes);
+
+	if (bytes > 1024) {
+		for (i = 0; (bytes / 1024) > 0 && i < length - 1; i++, bytes /= 1024)
+			dblBytes = bytes / 1024.0;
+	}
+
+	static char output[200];
+	sprintf_s(output, sizeof(output), "%.02lf %s", dblBytes, suffix[i]);
+	return output;
+}
+
+struct TraceAnchor
+{
+	u64 total_elapsed = 0;
+	u64 num_occurances = 0;
+	u64 bytes_processed = 0;
+	const char* label = nullptr;
+	int parent = -1;
+	int num_page_faults = 0;
+};
+
+struct TU_Profiler;
+
+struct GlobalProfiler
+{
+	static std::mutex profilers_mutex;
+	static std::vector<TU_Profiler*> profilers;
+
+	static void print_stats(u64 cpu_frequency);
+};
+
+const size_t num_anchors_per_TU = 128;
+
+struct TU_Profiler
+{
+	TU_Profiler()
+	{
+		std::lock_guard<std::mutex> guard(GlobalProfiler::profilers_mutex);
+		GlobalProfiler::profilers.push_back(this);
+	}
+
+	TraceAnchor anchors[num_anchors_per_TU];
+	int cur_anchor = -1;
+	OSHandler handler;
+};
+
+struct Trace
+{
+	Trace(const char* label, int _id, TU_Profiler* _profiler, u64 bytes_processed)
+	{
+		profiler = _profiler;
+		id = _id;
+		profiler->anchors[id].label = label;
+		profiler->anchors[id].parent = profiler->cur_anchor;
+		profiler->anchors[id].bytes_processed += bytes_processed;
+		start_page_faults = profiler->handler.GetPageFaults();
+		profiler->cur_anchor = id;
+		start = __rdtsc();
+	}
+
+	~Trace()
+	{
+		profiler->anchors[id].total_elapsed += __rdtsc() - start;
+		profiler->anchors[id].num_occurances++;
+		profiler->anchors[id].num_page_faults = profiler->handler.GetPageFaults() - start_page_faults;
+		profiler->cur_anchor = profiler->anchors[id].parent;
+	}
+
+	u64 start;
+	int id;
+	int start_page_faults;
+	TU_Profiler* profiler;
+};
+
+inline void GlobalProfiler::print_stats(u64 cpu_frequency)
+{
+	std::lock_guard<std::mutex> guard(GlobalProfiler::profilers_mutex);
+	std::string tabs;
+	tabs.reserve(8);
+	std::string percentStr;
+	percentStr.reserve(16);
+
+	for (size_t i = 0; i < profilers.size(); i++)
+	{
+		TU_Profiler* curprof = profilers[i];
+
+		if (curprof)
+		{
+			for (size_t j = 0; j < num_anchors_per_TU; j++)
+			{
+				TraceAnchor& curanch = curprof->anchors[j];
+
+				if (curanch.label)
+				{
+					int parent = curanch.parent;
+
+					if (parent >= 0)
+					{
+						double percent = 100.0 * static_cast<double>(curanch.total_elapsed) / static_cast<double>(curprof->anchors[parent].total_elapsed);
+
+						std::format_to(std::back_inserter(percentStr), " ({:.2f}%)", percent);
+					}
+
+					while (parent >= 0)
+					{
+						tabs.push_back('\t');
+						parent = curprof->anchors[parent].parent;
+					}
+
+					double seconds = static_cast<double>(curanch.total_elapsed) / cpu_frequency;
+					
+					std::cout << tabs << curanch.label << " total: " << curanch.total_elapsed << 
+						" (" << seconds << "s)";
+
+					std::cout << percentStr;
+
+					if (curanch.num_occurances > 1)
+					{
+						std::cout << ", samples: " << curanch.num_occurances
+							<< ", avg: " << curanch.total_elapsed / curanch.num_occurances;
+					}
+
+					if (curanch.bytes_processed > 0)
+					{
+						std::string bytestr = humanSize(curanch.bytes_processed);
+						std::string ratestr = humanSize(static_cast<u64>(static_cast<double>(curanch.bytes_processed) / seconds));
+
+						std::cout << ", bytes processed: " << bytestr <<
+							", throughput: " << ratestr << 's';
+					}
+
+					if (curanch.num_page_faults > 0)
+					{
+						std::cout << ", page faults: " << curanch.num_page_faults;
+					}
+
+					std::cout << std::endl;
+					tabs.clear();
+					percentStr.clear();
+				}
+			}
+		}
+	}
+}
+
+#define PASTE(a,b) a##b
+#define PASTE_EXPANDED(a,b) PASTE(a,b)
+
+#define PROFILED_FILE thread_local TU_Profiler add_PROFILED_FILE_to_the_top_of_this_file
+#define SCOPED_TRACE(LABEL, BYTES_PROCESSED) Trace PASTE_EXPANDED(temp_trace, __LINE__)(LABEL, __COUNTER__, &add_PROFILED_FILE_to_the_top_of_this_file, BYTES_PROCESSED)
---
+++ HaversineCPP/main.cpp
@@ -0,0 +1,310 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <random>
+#include <cassert>
+#include <argparse/argparse.hpp>
+#include "glaze/glaze.hpp"
+#include "Profiler.h"
+std::mutex GlobalProfiler::profilers_mutex;
+std::vector<TU_Profiler*> GlobalProfiler::profilers;
+
+PROFILED_FILE;
+struct HaversinePair
+{
+    double x0;
+    double y0;
+    double x1;
+    double y1;
+};
+
+struct HaversineData
+{
+    std::vector<HaversinePair> pairs;
+};
+
+/* ========================================================================
+   LISTING 65
+   ======================================================================== */
+
+typedef double f64;
+
+static f64 Square(f64 A)
+{
+    f64 Result = (A * A);
+    return Result;
+}
+
+static f64 RadiansFromDegrees(f64 Degrees)
+{
+    f64 Result = 0.01745329251994329577f * Degrees;
+    return Result;
+}
+
+// NOTE(casey): EarthRadius is generally expected to be 6372.8
+static f64 ReferenceHaversine(f64 X0, f64 Y0, f64 X1, f64 Y1, f64 EarthRadius)
+{
+    /* NOTE(casey): This is not meant to be a "good" way to calculate the Haversine distance.
+       Instead, it attempts to follow, as closely as possible, the formula used in the real-world
+       question on which these homework exercises are loosely based.
+    */
+
+    f64 lat1 = Y0;
+    f64 lat2 = Y1;
+    f64 lon1 = X0;
+    f64 lon2 = X1;
+    f64 dLat;
+    f64 dLon;
+    f64 a;
+    f64 c;
+
+    {
+        SCOPED_TRACE("Convert to radians", sizeof(u64) * 10);
+        dLat = RadiansFromDegrees(lat2 - lat1);
+        dLon = RadiansFromDegrees(lon2 - lon1);
+        lat1 = RadiansFromDegrees(lat1);
+        lat2 = RadiansFromDegrees(lat2);
+    }
+
+    {
+        SCOPED_TRACE("Calc a", sizeof(u64) * 5);
+        a = Square(sin(dLat / 2.0)) + cos(lat1) * cos(lat2) * Square(sin(dLon / 2));
+    }
+
+    {
+        SCOPED_TRACE("Calc c", sizeof(u64) * 2);
+        c = 2.0 * asin(sqrt(a));
+    }
+
+    f64 Result = EarthRadius * c;
+
+    return Result;
+}
+
+void GenData(const std::filesystem::path &input, int seed, int numpairs)
+{
+    std::ofstream outfile(input);
+    const std::size_t charsPerPair = sizeof("{\"x0\":102.1633205722960440, \"y0\":-24.9977499718717624, \"x1\":-14.3322557404258362, \"y1\":62.6708294856625940},");
+    std::string outstr = "{\"pairs\":[";
+
+    //outstr.reserve(charsPerPair * numpairs);
+    std::ostringstream outstream;
+
+    outstream << "{\"pairs\":[";
+
+    std::mt19937 gen(seed);
+    std::uniform_real_distribution lat(-180.0f, 180.0f);
+    std::uniform_real_distribution longtitude(-90.0f, 90.0f);//I can't remember if I got these right and I don't care enough to check
+
+    for (int i = 0; i < numpairs; i++)
+    {
+        outstream << "{\"x0\":" << lat(gen) << ", \"y0\":" << longtitude(gen) << ", \"x1\":" << lat(gen) << ", \"y1\":" << longtitude(gen) << "},";
+    }
+
+    outstream.seekp(-1, outstream.cur);
+    outstream << "]}";
+
+    outfile << outstream.str();
+}
+
+void run_reference_haversine(const std::filesystem::path& input)
+{
+    FILE* infile = nullptr;
+    SCOPED_TRACE("Total reference haversine", 0);
+    {
+        SCOPED_TRACE("Setup", 0);
+        errno_t err = fopen_s(&infile, input.string().c_str(), "rb");
+
+        assert(err == 0);
+    }
+
+    std::string contents;
+    HaversineData dat;
+    std::size_t fsize = 0;
+
+    { 
+        fsize = std::filesystem::file_size(input);
+        SCOPED_TRACE("Load file", fsize);
+        contents.resize(fsize);
+
+        if (infile)
+        {
+            size_t readNum = fread_s(&contents[0], contents.size(), 1, fsize, infile);
+
+            assert(readNum == fsize);
+
+            fclose(infile);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+
+    {
+        SCOPED_TRACE("Parse file", fsize);
+        auto data = glz::read_json<HaversineData>(contents);
+
+        if (!data)
+        {
+            std::cerr << data.error() << std::endl;
+            assert(false);
+        }
+
+        dat = data.value();
+    }
+
+    f64 sum = 0;
+    
+    {
+
+        for (size_t i = 0; i < dat.pairs.size(); i++)
+        {
+            SCOPED_TRACE("Run reference haversine", sizeof(HaversinePair));
+            const HaversinePair& curpair = dat.pairs[i];
+            sum += ReferenceHaversine(curpair.x0, curpair.y0, curpair.x1, curpair.y1, 6372.8);
+        }
+    }
+
+
+    {
+        SCOPED_TRACE("Print reference haversine results", 24);
+        std::cout << "Found a sum of: " << sum << std::endl;
+    }
+}
+
+void read_test(const std::filesystem::path& input, u64 cpu_frequency)
+{
+    u64 min = ULLONG_MAX;
+    u64 max = 0;
+
+    double average = 0;
+    double standard_deviation = 0;
+    const int iterations = 20;
+    u64 times[iterations];
+    auto fsize = std::filesystem::file_size(input);
+    double dsize = static_cast<double>(fsize);
+    double dfreq = static_cast<double>(cpu_frequency);
+
+    std::cout << "Testing file io for " << input << std::endl;
+
+    for (int i = 0; i < iterations; i++)
+    {
+        std::string contents;
+        contents.resize(fsize);
+        u64 start = __rdtsc();
+
+        FILE* infile = nullptr;
+        errno_t err = fopen_s(&infile, input.string().c_str(), "rb");
+
+        if (infile)
+        {
+            fread_s(&contents[0], contents.size(), fsize, 1, infile);
+
+            fclose(infile);
+        }
+
+        u64 dur = __rdtsc() - start;
+
+        min = min(min, dur);
+        max = max(max, dur);
+        times[i] = dur;
+        average += static_cast<double>(dur) / iterations;
+    }
+
+    for (int i = 0; i < iterations; i++)
+    {
+        double diff = (times[i] - average);
+        standard_deviation += diff * diff / iterations;
+    }
+
+    standard_deviation = sqrt(standard_deviation);
+
+    std::string bytestr = humanSize(fsize);
+
+    auto printTime = [=](u64 cycles, const char* label) {
+            double secs = static_cast<double>(cycles) / dfreq;
+            std::string rateStr = humanSize(static_cast<u64>(dsize / secs));
+
+            std::cout << label << cycles << ", " << secs << "s, " << rateStr << 's' << std::endl;
+        };
+
+    double sdsecs = static_cast<double>(standard_deviation) / dfreq;
+
+    std::cout << "Results for a " << bytestr << " file:" << std::endl;
+    printTime(min, "Min: ");
+    printTime(max, "Max: ");
+    printTime(average, "Avg: ");
+    std::cout << "SD:" << standard_deviation << ", " << sdsecs << "s, " << standard_deviation / average << '%' << std::endl;
+    printTime(times[0], "First: ");
+    printTime(times[1], "Second: ");
+    printTime(times[2], "Third: ");
+}
+
+int main(int argc, char* argv[])
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    argparse::ArgumentParser program("haversine");
+
+    program.add_argument("--gen", "-g")
+        .help("Generate haversine input data instead of processing it")
+        .flag();
+
+    program.add_argument("--test_read", "-r")
+        .help("Run a repitition test on file reads")
+        .flag();
+
+    program.add_argument("--numpairs", "-n")
+        .help("Number of pairs to generate")
+        .default_value(100)
+        .scan<'i', int>();
+
+    program.add_argument("--seed", "-s")
+        .help("Seed for generation")
+        .default_value(123)
+        .scan<'i', int>();
+
+    program.add_argument("--input_filename", "-i")
+        .help("Input file to generate/parse")
+        .default_value("input.json");
+
+    try
+    {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << program;
+        std::exit(1);
+    }
+
+    std::cout << "Page faults: " << add_PROFILED_FILE_to_the_top_of_this_file.handler.GetPageFaults() << std::endl;
+
+    u64 cpu_frequency = GuesstimateCPUFrequency();
+    auto input_path = std::filesystem::path(program.get<std::string>("--input_filename"));
+
+    std::cout << "CPU has a clock frequency of: " << cpu_frequency / 1'000'000'000.0 << "ghz" << std::endl;
+
+    if (program["--gen"] == true)
+    {
+        GenData(input_path, program.get<int>("--seed"), program.get<int>("--numpairs"));
+    }
+    else if (program["--test_read"] == true)
+    {
+        read_test(input_path, cpu_frequency);
+    }
+    else
+    {
+        run_reference_haversine(input_path);
+    }
+
+    std::cout << "Page faults: " << add_PROFILED_FILE_to_the_top_of_this_file.handler.GetPageFaults() << std::endl;
+
+    GlobalProfiler::print_stats(cpu_frequency);
+
+    std::cout << "In total, this program took " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start) << " to run" << std::endl;
+
+	return 0;
+}
